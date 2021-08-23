3f5bea89185838a7ccc20b93ba54a6d92e1763fd
gjtkdan7777 <tkdan7777@naver.com>
2021-08-23 22:45:39 +0900

    게시물 CRUD complete

--- back/nestjs-board-app/src/boards/boards.controller.ts
+++ back/nestjs-board-app/src/boards/boards.controller.ts
@@ -4,12 +4,14 @@ import {
   Delete,
   Get,
   Param,
+  ParseIntPipe,
   Patch,
   Post,
   UsePipes,
   ValidationPipe,
 } from '@nestjs/common'
-import { Board, BoardStatus } from './board.model'
+import { BoardStatus } from './board-status.enum'
+import { Board } from './board.entity'
 import { BoardsService } from './boards.service'
 import { CreateBoardDto } from './dto/create-board.dto'
 import { BoardStatusValidationPipe } from './pipes/board-status-validation.pipe'
@@ -19,31 +21,37 @@ export class BoardsController {
   constructor(private boardService: BoardsService) {}
 
   @Get('/')
-  getAllBoard(): Board[] {
+  getAllBoard(): Promise<Board[]> {
     return this.boardService.getAllBoards()
   }
 
+  // @Post()
+  // @UsePipes(ValidationPipe)
+  // createBoard(@Body() createBoardDto: CreateBoardDto): Board {
+  //   return this.boardService.createBoard(createBoardDto)
+  // }
+
   @Post()
   @UsePipes(ValidationPipe)
-  createBoard(@Body() createBoardDto: CreateBoardDto): Board {
+  createBoard(@Body() createBoardDto: CreateBoardDto): Promise<Board> {
     return this.boardService.createBoard(createBoardDto)
   }
 
   @Get('/:id')
-  getBoardById(@Param('id') id: string): Board {
+  getBoardByid(@Param('id') id: number): Promise<Board> {
     return this.boardService.getBoardById(id)
   }
 
   @Delete('/:id')
-  deleteBoard(@Param('id') id: string): void {
-    this.boardService.deleteBoard(id)
+  deleteBoard(@Param('id', ParseIntPipe) id: number): Promise<void> {
+    return this.boardService.deleteBoard(id)
   }
 
   @Patch('/:id/status')
   updateBoardStatus(
-    @Param('id') id: string,
+    @Param('id', ParseIntPipe) id: number,
     @Body('status', BoardStatusValidationPipe) status: BoardStatus,
-  ): Board {
+  ) {
     return this.boardService.updateBoardStatus(id, status)
   }
 }
