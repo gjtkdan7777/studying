fb94d3911f26b95f8420ec387d2d327c2179d74c
gjtkdan7777 <tkdan7777@naver.com>
2021-09-06 22:37:29 +0900

    nestjs 개요 끝 controller 부터 interceptor까

---
+++ back/nestjs-basic/src/cats/cats.controller.ts
@@ -0,0 +1,56 @@
+import { CatsService } from './cats.service';
+import {
+  Controller,
+  Delete,
+  Get,
+  HttpException,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post,
+  Put,
+  UseFilters,
+  UseInterceptors,
+} from '@nestjs/common';
+import { HttpExceptionFilter } from 'src/common/exceptions/http-exception.filter';
+import { SuccessInterceptor } from 'src/common/interceptors/success.interceptor';
+
+@Controller('cats')
+@UseInterceptors(SuccessInterceptor)
+@UseFilters(HttpExceptionFilter)
+export class CatsController {
+  constructor(private readonly catsService: CatsService) {}
+
+  // cats/
+  @Get()
+  getAllCat() {
+    console.log('hello controller');
+    return { cats: 'get all cat api' };
+  }
+
+  // cats/:id
+  @Get(':id')
+  getOneCat(@Param('id', ParseIntPipe) param: number) {
+    return 'get one cat api';
+  }
+
+  @Post()
+  createCat() {
+    return 'create cat api';
+  }
+
+  @Put(':id')
+  updateCat() {
+    return 'update cat';
+  }
+
+  @Patch(':id')
+  updatePartialCat() {
+    return 'update cat';
+  }
+
+  @Delete(':id')
+  deleteCat() {
+    return 'delete service';
+  }
+}
